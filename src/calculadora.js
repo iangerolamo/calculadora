116d3626e4bcdbbe94f21e2cb1ab6d936cff2da7
iangerolamo <67077374+iangerolamo@users.noreply.github.com>
2020-07-29 12:53:15 -0300

    Mudança no design da calculadora

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,7 +1,48 @@
 import React from 'react';
+import './calculadora.css';
+import { Button, Row, Col, Divider } from 'antd';
+
 
 function Calculadora () {
-  return <h1>Olá mundo!</h1>
+  return (
+    <div className="main">
+      <form name="MyForm">
+        <input type="text" name="textview" className="textview"/>
+      </form>
+      <table>
+        <tr>
+          <td><input type="button" className="button" value="√"/></td>
+          <td><input type="button" className="button" value="x²"/></td>
+          <td><input type="button" className="button" value="⌫"/></td>
+          <td><input type="button" className="button" value="÷"/></td>
+        </tr>
+        <tr>
+          <td><input type="button" class="button" value="7"/></td>
+          <td><input type="button" class="button" value="8"/></td>
+          <td><input type="button" class="button" value="9"/></td>
+          <td><input type="button" class="button" value="X"/></td>
+        </tr>
+        <tr>
+          <td><input type="button" class="button" value="4"/></td>
+          <td><input type="button" class="button" value="5"/></td>
+          <td><input type="button" class="button" value="6"/></td>
+          <td><input type="button" class="button" value="-"/></td>
+        </tr>
+        <tr>
+          <td><input type="button" class="button" value="1"/></td>
+          <td><input type="button" class="button" value="2"/></td>
+          <td><input type="button" class="button" value="3"/></td>
+          <td><input type="button" class="button" value="+"/></td>
+        </tr>
+        <tr>
+          <td><input type="button" class="button" value="C"/></td>
+          <td><input type="button" class="button" value="0"/></td>
+          <td><input type="button" class="button" value="."/></td>
+          <td><input type="button" class="button" value="="/></td>
+        </tr>
+      </table>
+    </div>
+  )
 }
 
 export default Calculadora
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Calculadora from './calculadora'
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
     <Calculadora />,
